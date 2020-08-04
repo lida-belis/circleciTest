561c5b619d1f266f88f0f18d4a7ef24fbd6c4a41
lida-belis <bellida88@mail.ru>
2020-08-04 21:19:25 +0300

    added firefoxDriver

--- src/test/java/org/teachmeskills/aqa5/MainPageTest.java
+++ src/test/java/org/teachmeskills/aqa5/MainPageTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,6 +18,20 @@ public class MainPageTest {
         WebDriverManager.getInstance(driverManagerType).setup();
 
         webDriver = new ChromeDriver();
+        webDriver.get("https://www.google.com");
+
+        System.out.println(webDriver.getTitle());
+        Assert.assertEquals(webDriver.getTitle(), "Google");
+        webDriver.close();
+    }
+
+    @Test
+    public void openFirefoxPage() {
+        System.out.println("Selenium has been started!");
+        DriverManagerType driverManagerType = DriverManagerType.CHROME;
+        WebDriverManager.getInstance(driverManagerType).setup();
+
+        webDriver = new FirefoxDriver();
         webDriver.get("https://www.google.com");
 
         System.out.println(webDriver.getTitle());
