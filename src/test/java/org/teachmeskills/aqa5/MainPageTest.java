e60773ad156fc8e8d9f41d2622e52b49ca71b185
lida-belis <bellida88@mail.ru>
2020-08-04 20:13:12 +0300

    init

---
+++ src/test/java/org/teachmeskills/aqa5/MainPageTest.java
@@ -0,0 +1,26 @@
+package org.teachmeskills.aqa5;
+
+import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainPageTest {
+    WebDriver webDriver;
+
+    @Test
+    public void openChromePage() {
+        System.out.println("Selenium has been started!");
+        DriverManagerType driverManagerType = DriverManagerType.CHROME;
+        WebDriverManager.getInstance(driverManagerType).setup();
+
+        webDriver = new ChromeDriver();
+        webDriver.get("https://www.google.com");
+
+        System.out.println(webDriver.getTitle());
+        Assert.assertEquals(webDriver.getTitle(), "Google");
+        webDriver.close();
+    }
+}
